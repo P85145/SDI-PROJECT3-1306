9cd2cffcb2a03af01919660a0bb9b60f0110e5c7
P85145 <CeCe@Cerelias-MacBook-Pro.local>
2013-06-20 01:06:57 -0400

    Updated HTML and Added Variables

---
+++ williams_cerelia_assignment3 copy.js
@@ -0,0 +1,16 @@
+//alert("JavaScript works!");
+
+// Cerelia Williams
+// SDI 1603
+// Project 3
+// Video Game Play
+ 
+ 
+// Variables
+ 
+var gameName = "Sonic The Hedgehog";
+var numOfPoints = 100;
+var hasPoints = true;
+var playerNames = ["Corey", "Colin", "Noah", "SJ"];
+ 
+ 
